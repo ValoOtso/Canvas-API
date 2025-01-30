9a2cff3c3d4ffcd4eb00a2d0da62a543c1a584c3
Tuukka Heiskanen <tuukkaheiskanen5@gmail.com>
2025-01-30 13:42:57 +0200

    hahmotelma

---
+++ script.js
@@ -0,0 +1,23 @@
+var peliHahmo;
+var laatta = []
+var pisteet;
+
+//pelin käynnistys
+function aloitaPeli() {
+
+}
+
+//canvas
+var peliAlue {
+
+}
+
+//komponentti konstruktori
+function Komponentti() {
+
+}
+
+//pelialueen päivitys
+function paivitaPeliAlue() {
+
+}
