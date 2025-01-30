1415b48723d179f59f9acf8a021198b7f8af19ac
Tuukka Heiskanen <tuukkaheiskanen5@gmail.com>
2025-01-30 14:13:46 +0200

    canvas

--- script.js
+++ script.js
@@ -10,8 +10,14 @@ function aloitaPeli() {
 }
 
 //canvas
-var peliAlue {
-
+var peliAlue = {
+    canvas : document.createElement('canvas'),
+    aloita : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext('2d');
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+}
 }
 
 //komponentti konstruktori
