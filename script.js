885986b33c5f073a78c2bb21796f093db93763ef
Tuukka Heiskanen <tuukkaheiskanen5@gmail.com>
2025-01-30 14:44:57 +0200

    päivitys

--- script.js
+++ script.js
@@ -17,6 +17,20 @@ var peliAlue = {
         this.canvas.height = 480;
         this.context = this.canvas.getContext('2d');
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(paivitaPeliAlue, 20);
+        window.addEventListener('keydown', function (e) {
+            peliAlue.avaimet = (peliAlue.kavaimet || []);
+            peliAlue.avaimet[e.key] = true
+        })
+        window.addEventListener('keyup', function (e) {
+            peliAlue.avaimet[e.key] = false;
+        })
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop : function() {
+        clearInterval(this.interval);
     }
 }
 
@@ -27,5 +41,7 @@ function Komponentti() {
 
 //pelialueen päivitys
 function paivitaPeliAlue() {
-
+    peliAlue.clear();
+    if (peliAlue.avaimet && peliAlue.avaimet['ArrowRight']) {peliHahmo.speedX = 1;}
+    if (peliAlue.avaimet && peliAlue.avaimet['ArrowLeft']) {peliHahmo.speedX = -1;}
 }
